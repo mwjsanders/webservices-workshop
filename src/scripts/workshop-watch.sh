b36da254845aed2703a24b9c0f59955d890f1591
Anton Bakker <anton.bakker@kadaster.nl>
2020-03-10 23:23:26 +0100

    removed unnecessary files

--- src/scripts/workshop-watch.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-WORKSHOP_FILE="./src/WORKSHOP.md"
-while inotifywait -e close_write "$WORKSHOP_FILE"; do cp -r src/images docs/ && pandoc -c pandoc.css "$WORKSHOP_FILE" -o docs/index.html; done
