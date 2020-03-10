ad0d603a900f659b7f77905e72205f6c2c804fd7
Anton Bakker <anton.bakker@kadaster.nl>
2020-03-10 23:20:49 +0100

    removed unnecessary files

--- src/scripts/workshop-build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-WORKSHOP_FILE="src/WORKSHOP.md"
-cp -r src/images docs/
-title=$(pcregrep -o1 '^<\!--\sTITLE:\s(.*)\s-->' "$WORKSHOP_FILE" | head -n1)
-pandoc  -c pandoc.css "$WORKSHOP_FILE" -o docs/index.html --metadata title="$title"
